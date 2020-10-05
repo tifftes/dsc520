06792c9e78306912b759c43fb784d89b6c0898f7
tifftes <66386992+tifftes@users.noreply.github.com>
2020-10-04 17:23:45 -0700

    Week 5 Update 2

--- exercise_9_TesoroTiffany.R
+++ exercise_9_TesoroTiffany.R
@@ -15,3 +15,36 @@ install.packages("ggplot2"); install.packages("polycor")
 ## Load packages for correlation analysis in R
 library(book); library(ggm); library(ggplot2);
 library(Hmisc); library(polycor)
+
+## r covariance
+cov(survey_df[])
+
+## calculates correlation for all dataframe
+cor(survey_df, use = "complete.obs", method = "pearson")
+
+## r pair correlation
+cor.test(survey_df$TimeReading, survey_df$Happiness, method = "pearson")
+
+## r pair negative correlation 
+cor.test(survey_df$TimeReading, survey_df$Happiness, alternative = "less", method = "pearson")
+
+## r confidence interval 99% correlation
+cor.test(survey_df$TimeReading, survey_df$Happiness, alternative = "less", method = "pearson", conf.level = 0.99)
+
+## r coefficient of determination
+TimeTV.lm = lm(TimeTV ~ Happiness, data=survey_df)
+summary(TimeTV.lm)$r.squared 
+
+## r correlation coefficient
+TimeTV = survey_df$TimeTV  
+Happiness = survey_df$Happiness    
+cor(TimeTV, Happiness) 
+
+## r partial correlation - control TimeReading
+pcor(c("Happiness", "TimeTV", "TimeReading"), var(survey_df))
+
+## r partial correlation - control Gender
+pcor(c("Happiness", "TimeTV", "Gender"), var(survey_df))
+
+## r partial correlation - control TimeTV
+pcor(c("Happiness", "TimeReading", "TimeTV"), var(survey_df))
